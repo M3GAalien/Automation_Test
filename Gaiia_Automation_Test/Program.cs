1d92c8dfa7a935829abc97c94cf98eeadb654a12
M3GAalien <55897035+M3GAalien@users.noreply.github.com>
2025-12-02 22:25:56 +0000

    Automated commit

--- Gaiia_Automation_Test/Program.cs
+++ Gaiia_Automation_Test/Program.cs
@@ -4,7 +4,7 @@ using TextCopy;
 using System.Diagnostics;
 
 
-bool debug = true; // SET FALSE BEFORE BUILDING - not allowed to do threads in github codespaces 
+bool debug = false; // SET FALSE BEFORE BUILDING - not allowed to do threads in github codespaces 
 bool slowMode = true; // add timed delays for *asthetic* reasons
 int delay = 1000; // delay to add in miliseconds
 
